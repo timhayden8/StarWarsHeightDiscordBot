32fc87fc0968303a32b47a1f2b313d85a7cfc4c5
timhayden8 <119264210+timhayden8@users.noreply.github.com>
2023-03-29 20:45:46 -0600

    Update StarWarsHeight.py

--- StarWarsHeight.py
+++ StarWarsHeight.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import discord
+#Connect to discord API and start bot. Bot listens for "!starwarsheight". It then responds then waits for a response. 
+#The response is captured as a variable.
 intents = discord.Intents.all()
 client = discord.Client(command_prefix='!', intents=intents)
 @client.event
@@ -15,6 +17,8 @@ async def on_message(message):
         message = await client.wait_for('message')
         input = message.content
         try:
+#A get request is sent to the star war's API, pulling down a list of characters. The response is then parsed for the character name taken from discord
+#From this dictionary, the character's height is saved as a variable and sent to the discord API.
             r = requests.get("https://swapi.dev/api/people")
             r=r.content
             r=json.loads(r)
@@ -23,6 +27,7 @@ async def on_message(message):
                 if i['name'].lower() == input.lower():
                     height = i['height']
             await message.channel.send(f'{input} is {height} cm tall.')
+#If the height or character can't be found, the bot sends this error message.
         except:
             await message.channel.send("Sorry, this character isn't in the SWAPI Database.")
-client.run('YOUR BOT TOKEN HERE')
+client.run("<YOUR BOT TOKEN HERE>")
